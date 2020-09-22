f832c862732098aca539b710f758e511900f0943
presk <presseauk@gmail.com>
2020-09-22 15:59:29 -0400

    First implementation
    Needs bugfixing

--- bitsort.java
+++ bitsort.java
@@ -7,6 +7,9 @@ class Bitsort
     private int[] intArray;
     private float[] floatArray;
     private String type;
+
+
+
     public Bitsort(){}
 
     public Bitsort(float[] arr)
@@ -18,6 +21,7 @@ class Bitsort
         {
             this.bitArray[i] = this.numToBit(arr[i]);
         }
+        this.sorted();
     }
     public Bitsort(int[] arr)
     {
@@ -41,6 +45,7 @@ class Bitsort
         {
             this.bitArray[i] = this.numToBit(arr[i], max);
         }
+        this.sorted();
     }
     public static void main(String[] args) 
     {
@@ -51,10 +56,19 @@ class Bitsort
             arr[i] = rand.nextInt(100) - 50;
         }
         Bitsort b = new Bitsort(arr);
+        int[] sArr = b.getIntSorted();
         for(int i = 0; i < arr.length; i++)
         {
-            System.out.println(arr[i] + " : " + b.bitArray[i]);
+            System.out.println(sArr[i] + " : " + b.bitArray[i]);
         }
+    }
+    public int[] getIntSorted()
+    {
+        return this.intArray;
+    }
+    public float[] getFloatSorted()
+    {
+        return this.floatArray;
     }
     private String numToBit(float f)
     {
@@ -82,5 +96,65 @@ class Bitsort
             }
             return "1" + pad + bitString;
         }
+    }
+    private void sorted()
+    {
+        int swapPos = 0;
+        for(int i = 0; i < bitArray.length; i++)
+        {
+            if(bitArray[i].charAt(0) == 1)
+            {
+                swap(i, swapPos);
+                swapPos ++;
+            }
+        }
+        innerSort(0, swapPos - 1, 1, 1);
+        innerSort(swapPos, this.bitArray.length - 1, 1, 0);
+    }
+    private void innerSort(int start, int end, int bit, int order)
+    {
+        int swapPos = start;
+        for(int i = start; i <= end; i++)
+        {
+            if(bitArray[i].charAt(bit) == order)
+            {
+                if(i != swapPos)
+                    swap(i, swapPos);
+                swapPos ++;
+            }
+        }
+        if(swapPos == start)
+        {
+            swapPos = end + 1;
+        }
+        if(bit < bitArray[0].length() - 1)
+        {
+            if(start < swapPos - 1)
+            {
+                innerSort(start, swapPos - 1, bit + 1, order);
+            }
+            if(swapPos < end)
+            {
+                innerSort(swapPos, end, bit + 1, order);
+            }
+        }
+    }
+    private void swap(int i, int swapPos)
+    {
+        if(type == "int")
+        {
+            int temp = this.intArray[swapPos];
+            this.intArray[swapPos] = this.intArray[i];
+            this.intArray[i] = temp;
+        }
+        else if(type == "float")
+        {
+            float temp = this.floatArray[swapPos];
+            this.floatArray[swapPos] = this.floatArray[i];
+            this.floatArray[i] = temp;
+        }
+        String temp = this.bitArray[swapPos];
+        this.bitArray[swapPos] = this.bitArray[i];
+        this.bitArray[i] = temp;
     }
 }
