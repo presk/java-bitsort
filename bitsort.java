b67a567e7281d358fb09e194561d0026f11861d2
presk <presseauk@gmail.com>
2020-09-24 15:09:15 -0400

    final version before optimization
    Complete version with comments and proper time analysis before attempts at optimization.

--- bitsort.java
+++ bitsort.java
@@ -35,6 +35,7 @@ class Bitsort
         {
             if(arr[i] != 0)
             {
+                //Looks for the minimum amount of bits to represent all integers in the array
                 int temp = 32 - Integer.numberOfLeadingZeros(Math.abs(arr[i]));
                 if( temp > max)
                 {
@@ -52,39 +53,58 @@ class Bitsort
     public static void main(String[] args) 
     {
         Random rand = new Random();
-        /*int[] arr = new int [1000000];
-        for(int i = 0; i < arr.length; i++)
+
+        int[] intArr = new int [1000000];
+        for(int i = 0; i < intArr.length; i++)
         {
-            arr[i] = rand.nextInt(10000) - 5000;
-        }*/
-        float[] arr = new float [1000000];
-        for(int i = 0; i < arr.length; i++)
-        {
-            arr[i] = (rand.nextFloat() - 0.5f) * (10000000000f);
+            intArr[i] = rand.nextInt(1000000) - 500000;
         }
-        Bitsort b = new Bitsort(arr);
+        
+        float[] floatArr = new float [1000000];
+        for(int i = 0; i < floatArr.length; i++)
+        {
+            floatArr[i] = (rand.nextFloat() - 0.5f) * (10000000000f);
+        }
+        
+
         long startTime = System.nanoTime();
-        float[] sArr = b.sorted(arr);
-        long endTime = System.nanoTime();
-        long timeElapsed1 = endTime - startTime;
-        System.out.println("Time elapsed bitsort: " + timeElapsed1);
+        int[] sArr = new Bitsort(intArr).sorted(intArr);
+
+        long timeElapsed1 = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
+        System.out.println("Time elapsed bitsort: " + timeElapsed1 + " ms");
 
 
         startTime = System.nanoTime();
-        Arrays.sort(arr);
-        endTime = System.nanoTime();
-        long timeElapsed2 = endTime - startTime;
-        System.out.println("Time elapsed java sort: " + timeElapsed2);
+        Arrays.sort(intArr);
 
-
-        //int[] ssarr = new Bitsort(arr).sorted(arr);
-        /*for(int i = 0; i < sArr.length; i++)
-        {
-            System.out.println(sArr[i] + "      " + arr[i]+ " : " + b.bitArray[i]);
-        }*/
-        if(Arrays.equals(sArr, arr))
+        long timeElapsed2 = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
+        System.out.println("Time elapsed java sort: " + timeElapsed2 + " ms");
+        System.out.println("Delta bitsort vs sort: " + (timeElapsed1 - timeElapsed2) + " ms");
+        System.out.println("Ratio bitsort vs sort: ~" + (timeElapsed1 / timeElapsed2) + "x slower");
+        if(Arrays.equals(sArr, intArr))
         {
             System.out.println("true");
+        }
+
+        startTime = System.nanoTime();
+        float[] sfArr = new Bitsort(floatArr).sorted(floatArr);
+
+
+        timeElapsed1 = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
+        System.out.println("Time elapsed bitsort: " + timeElapsed1 + " ms");
+
+
+        startTime = System.nanoTime();
+        Arrays.sort(floatArr);
+
+
+        timeElapsed2 = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
+        System.out.println("Time elapsed java sort: " + timeElapsed2 + " ms");
+        System.out.println("Delta bitsort vs sort: " + (timeElapsed1 - timeElapsed2) + " ms");
+        System.out.println("Ratio bitsort vs sort: ~" + (timeElapsed1 / timeElapsed2) + "x slower");
+        if(Arrays.equals(sfArr, floatArr))
+        {
+            System.out.println("Arrays are equals: true");
         }
     }
     
@@ -96,22 +116,26 @@ class Bitsort
     {
         return this.floatArray;
     }
+    /*Returns the bit representation of a float. (IEEE-754)
+    */
     private String numToBit(float f)
     {
 
         String fString = Integer.toBinaryString(Float.floatToIntBits(f));
-        /*if (f < 0.0f)
-        {
-            System.out.println("Is < 0, bitsize: " + temp.length());
-        }*/
-        //floatToIntBits seems to return a 31 bit string for positives
-        // and 32 bit string for negatives
+
+        /*floatToIntBits returns 31 bits for positives and 32 bits for negatives. 
+        * Positives are then padded with a 0 to the left to represent the sign bit.
+        */
         if (f >= 0.0f)
         {
             fString = "0" + fString;
         }
         return fString;
     }
+    /*Returns the bit representation of the absolute value of an integer with a 
+    * sign bit on the left (0->+, 1->-). It also pads integers to have representations
+    *  of equal size. 
+    */
     private String numToBit(int i, int max)
     {
         if( i >= 0)
@@ -135,7 +159,13 @@ class Bitsort
             return "1" + pad + bitString;
         }
     }
-
+    /*Splits the array into positive and negatives,
+    * then recursively sorts the sub-arrays.
+    * Sub-arrays of negatives are sorted in descending order,
+    * while sub-arrays of positives are sorted in ascending order.
+    * Ascending: 1's to the left
+    * Descending: 0's to the left
+    */
     private void sort()
     {
         int swapPos = 0;
@@ -150,7 +180,9 @@ class Bitsort
         innerSort(0, swapPos - 1, 1, '1');
         innerSort(swapPos, this.bitArray.length - 1, 1, '0');
     }
-
+    /*Sorts sub-arrays by splitting 1's and 0's and placing
+    * the order type bits to the left (for example: 111000 or 000111). 
+    */
     private void innerSort(int start, int end, int bit, char order)
     {
         int swapPos = start;
@@ -163,10 +195,14 @@ class Bitsort
                 swapPos ++;
             }
         }
+        /*Checks if the sub-array had identical bits different from order
+        * Then changes the swap position to the end of the sub-array*/
         if(swapPos == start)
         {
             swapPos = end + 1;
         }
+        /*Checks if we've reached the last bit, 
+        *then checks if the sub-array's size is bigger than 1*/
         if(bit < bitArray[0].length() - 1)
         {
             if(start < swapPos - 1)
@@ -179,7 +215,8 @@ class Bitsort
             }
         }
     }
-
+    /*Swaps numbers in both their bit representation array 
+    * and the copy of the original array */
     private void swap(int i, int swapPos)
     {
         if(type.equals("int"))
